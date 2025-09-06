de03f41099b9781006f2470bc9d2a1b4d0c1eb1f
drawiks <147206917+drawiks@users.noreply.github.com>
2025-09-06 16:12:00 +0300

    hotfix

--- src/utils/cooldown.py
+++ src/utils/cooldown.py
@@ -1,27 +1,26 @@
-
 from twitchAPI.chat import ChatCommand
 import time
 
 def cooldown(seconds=30, per_user=True):
     ignore_users = ["drawksr69", "alaqu1337"]
+    cooldowns = {}
     def wrapper(func):
-        async def inner(self, cmd: ChatCommand, *args, **kwargs):
+        async def inner(cmd: ChatCommand, *args, **kwargs):
             if cmd.user.name in ignore_users:
-                return await func(self, cmd, *args, **kwargs)
-            
+                return await func(cmd, *args, **kwargs)
+
             key = (cmd.user.name, func.__name__) if per_user else func.__name__
             now = time.time()
-            
-            if not hasattr(self, "_cooldowns"):
-                self._cooldowns = {}
-                last_used = self._cooldowns.get(key, 0)
-                if now - last_used < seconds:
-                    wait = round(seconds - (now - last_used), 1)
+            last_used = cooldowns.get(key, 0)
+            elapsed = now - last_used
+
+            if elapsed < seconds:
+                wait = round(seconds - elapsed, 1)
                 await cmd.reply(f"Подожди {wait} сек!")
                 return
-                
-            self._cooldowns[key] = now
-                
-            return await func(self, cmd, *args, **kwargs)
+
+            cooldowns[key] = now
+
+            return await func(cmd, *args, **kwargs)
         return inner
     return wrapper
