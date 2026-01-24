9369d7d8ff8d6a5b8f879363951a43f3be418b61
drawiks <147206917+drawiks@users.noreply.github.com>
2026-01-24 17:01:48 +0200

    refactor translate util

--- src/utils/translate.py
+++ src/utils/translate.py
@@ -1,6 +1,16 @@
 
 from deep_translator import GoogleTranslator
+from langdetect import detect, DetectorFactory
 
-def get_translate(text: str, target: str = "ru"):
-    text = GoogleTranslator(source="auto", target=target).translate(text)
-    return text
+DetectorFactory.seed = 0
+
+def get_translate(text: str):
+    try:
+        source_lang = detect(text)
+        target_lang = "en" if source_lang == "ru" else "ru"
+        
+        translation = GoogleTranslator(source="auto", target=target_lang).translate(text)
+        return translation
+    
+    except Exception as e:
+        return f"Ошибка перевода: {e}"
