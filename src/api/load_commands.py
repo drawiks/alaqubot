fd705c28436c631620bfdf1b479f0d830866e4f7
drawiks <147206917+drawiks@users.noreply.github.com>
2026-01-27 14:12:32 +0200

    fix: change ports in local api

--- src/api/load_commands.py
+++ src/api/load_commands.py
@@ -3,7 +3,7 @@ import requests
 
 def load_commands():
     try:
-        response = requests.get("http://127.0.0.1:9000/commands")
+        response = requests.get("http://127.0.0.1:9090/commands")
         if response.status_code == 200:
             return response.json() 
     except Exception as e:
