d490c679c261eab792c5a67daed7bd3019beee0c
drawiks <147206917+drawiks@users.noreply.github.com>
2026-01-27 14:18:31 +0200

    minor fix

--- src/api/load_commands.py
+++ src/api/load_commands.py
@@ -4,7 +4,7 @@ import requests
 def load_commands():
     try:
         response = requests.get("http://127.0.0.1:9090/commands")
-        if response:
+        if response.status_code == 200:
             return response.json() 
     except Exception as e:
         pass
--- src/api/load_users.py
+++ src/api/load_users.py
@@ -4,7 +4,7 @@ import requests
 def load_users():
     try:
         response = requests.get("http://127.0.0.1:9090/users")
-        if response:
+        if response.status_code == 200:
             return response.json() 
     except Exception as e:
         pass
