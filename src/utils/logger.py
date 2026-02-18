787e0d158386f9dc45c288082780578379f39fd5
drawiks <147206917+drawiks@users.noreply.github.com>
2026-02-18 12:02:42 +0200

    chore: update dlogger configuration to support new version

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -4,8 +4,10 @@ from src.config import LOG_PATH
 from dlogger import logger
 
 logger.configure(
+    level="TRACE",
     log_file=LOG_PATH,
     rotation="20MB",
     retention="7 days",
-    compression=True
+    compression=True,
+    time_format="%d.%m.%Y %H:%M"
 )
