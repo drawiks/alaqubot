1a6e494e728154b1a944fe12a569ae6f95e1287b
drawiks <147206917+drawiks@users.noreply.github.com>
2026-02-20 11:19:21 +0200

    chore: add command logging

--- src/utils/cooldown.py
+++ src/utils/cooldown.py
@@ -1,7 +1,9 @@
+
 from functools import wraps
 import time
 
 from .cache import CacheManager
+from .logger import logger
 
 def cooldown(seconds=30, per_user=True):
     cooldowns = CacheManager(maxsize=1000, ttl=seconds)
@@ -9,18 +11,20 @@ def cooldown(seconds=30, per_user=True):
     def decorator(func):
         @wraps(func)
         async def wrapper(self, cmd, *args, **kwargs):
+            logger.trace(f"cmd: {func.__name__} | {cmd.user.name}")
+            
             ignore_users = self.client.users
             if cmd.user.name in ignore_users:
                 return await func(self, cmd, *args, **kwargs)
-
+            
             key = f"{cmd.user.name}:{func.__name__}" if per_user else func.__name__
             last = cooldowns.get_cache(key)
-
+            
             if last is not None:
                 wait = round(seconds - (time.time() - last), 1)
                 await cmd.reply(f"Подожди {wait} сек!")
                 return
-
+            
             cooldowns.set_cache(key, time.time())
             return await func(self, cmd, *args, **kwargs)
         return wrapper
